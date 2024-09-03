be22403bef1d951b71fdeefd384bdcfdf19fadc3
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-03 16:12:27 +0900

    Feat: input 컴포넌트 삼항연산자 수정

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -21,19 +21,26 @@ export default function Input({ status }: InputProps) {
   return (
     <>
       {
-        status === 'disabled'
-        ?
-        <div className={getInputStatus()}>
-          <label htmlFor="text" className="body-sm">타이틀</label>
-          <input type="text" id="text" className="body-sm" disabled placeholder="내용을 입력해 주세요." />
-          <span className="body-xs">내용을 입력해 주세요.</span>
-        </div>
-        :
-        <div className={getInputStatus()}>
-          <label htmlFor="text" className="body-sm">타이틀</label>
-          <input type="text" id="text" className="body-sm" placeholder="내용을 입력해 주세요." />
-          <span className="body-xs">내용을 입력해 주세요.</span>
-        </div>
+        status === 'search'
+          ?
+          <div className={styles.inputSearch}>
+            <label htmlFor="search" className="visually-hidden">검색어</label>
+            <input type="text" id="search" className="body-sm" placeholder="검색어를 입력해 주세요." />
+          </div>
+          :
+          status === 'disabled'
+            ?
+            <div className={getInputStatus()}>
+              <label htmlFor="text" className="body-sm">타이틀</label>
+              <input type="text" id="text" className="body-sm" disabled placeholder="내용을 입력해 주세요." />
+              <span className="body-xs">내용을 입력해 주세요.</span>
+            </div>
+            :
+            <div className={getInputStatus()}>
+              <label htmlFor="text" className="body-sm">타이틀</label>
+              <input type="text" id="text" className="body-sm" placeholder="내용을 입력해 주세요." />
+              <span className="body-xs">내용을 입력해 주세요.</span>
+            </div>
       }
     </>
   )
