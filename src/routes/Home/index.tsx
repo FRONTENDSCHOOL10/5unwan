61aa447a0bed907a9d3db8845ad60e5f2f053def
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-05 14:45:54 +0900

    Feat: 운동데이터 불러오기

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,9 +1,19 @@
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { getExercises, logout } from '@/api/pocketbase'
+import { useNavigate, useOutletContext, Link } from 'react-router-dom';
 import { UserContext } from "@/routes/PrivateRoute";
-import { logout } from "@/api/pocketbase";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+interface Exercise {
+  id: string;
+  type: string;
+  title: string;
+  img_url: string;
+  link: string;
+}
+
 export default function Home() {
+  const [exercises, setExercises] = useState<Exercise[]>([]);
   const { user } = useOutletContext<UserContext>();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
@@ -18,17 +28,47 @@ export default function Home() {
     },
   });
 
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const data = await getExercises();
+        setExercises(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchExercises();
+  }, []);
+
+  console.log(exercises);
+
   return (
-    <div>
-      <p>현재 사용자: {user?.email}</p>
-      <br />
-      <button
-        onClick={async () => {
+    <>
+      <div>
+        <p>현재 사용자: {user?.email}</p>
+        <br />
+        <button onClick={async () => {
           await logoutMutation.mutateAsync();
-        }}
-      >
-        로그아웃
-      </button>
-    </div>
-  );
+        }}>
+          로그아웃
+        </button>
+      </div>
+      <ul>
+        {
+          exercises.map((exercise) => (
+            <li key={ exercise.id }>{ exercise.type }</li>
+          ))
+        }
+      </ul>
+      {
+        exercises.map((exercise) => (
+          <Link to={exercise.link}>
+            <img src={exercise.img_url} alt="" />
+            <h3>{ exercise.title}</h3>
+          </Link>
+        ))
+
+      }
+    </>
+  )
 }
