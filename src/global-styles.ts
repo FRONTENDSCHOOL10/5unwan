921d65d43719d04cb80bc7e54604ca316d2f5dbd
seulbinim <skfo0827@naver.com>
2024-09-09 16:26:20 +0900

    Style: 다크모드 스타일 및 테마

---
+++ src/global-styles.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+interface ThemeInterface {
+  theme: {
+    body: string;
+    text: string;
+    toggleBackground: string;
+    mainColor: string;
+    navBar: string;
+  };
+}
+
+export const GlobalStyle = createGlobalStyle<ThemeInterface>`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'NanumSquare', sans-serif;
+        background: ${({ theme }) => theme.body};
+        color: ${({ theme }) => theme.text};
+        transition: all 0.5s ease-in-out;
+    }
+    button {
+        background: none;
+        cursor: pointer;
+        border: none;
+        outline: none;
+        transition: all 0.5s ease-in-out;
+    }
+    ol, ul, li {
+        list-style: none;
+    }
+    a {
+        text-decoration: none;
+        cursor: pointer;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+    body: '#fcfcfc',
+    text: '#363537',
+    toggleBackground: '#fcfcfc',
+    mainColor: '#e6328d',
+    navBar: '#fcfcfc',
+  };
+  
+  export const darkTheme = {
+    body: '#252424',
+    text: '#fcfcfc',
+    toggleBackground: '#3b3b3b',
+    mainColor: '#fcfcfc',
+    navBar: '#303030',
+  };
+  
+  export type Theme = typeof lightTheme;
