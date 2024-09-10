f20b0e8b295688796754de9f32aa4a78b092300e
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-11 01:29:31 +0900

    Refactor: Input 컴포넌트 리팩토링

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react';
 import styles from './input.module.css';
 
 interface InputProps {
@@ -5,46 +6,50 @@ interface InputProps {
   isDark?: boolean
 }
 
-export default function Input({ status, isDark }: InputProps) {
-  function getInputStatus() {
-    const darkClass = isDark ? styles["is-dark"] : '';
+function getInputStatus(status :string, isDark :boolean) {
+  const darkClass = isDark ? styles["is-dark"] : '';
 
-    switch (status) {
-      case 'search':
-        return `${styles["input-search"]} ${darkClass}`;
-      case 'disabled':
-        return `${styles["input-text"]} ${darkClass}`;
-      case 'text':
-        return `${styles["input-text"]} ${darkClass}`;
-      default:
-        return `${styles["input-text"]} ${darkClass}`;
-    }
+  switch (status) {
+    case 'search':
+      return `${styles["input-search"]} ${darkClass}`;
+    case 'disabled':
+      return `${styles["input-text"]} ${darkClass}`;
+    case 'text':
+      return `${styles["input-text"]} ${darkClass}`;
+    default:
+      return `${styles["input-text"]} ${darkClass}`;
+  }
+}
+
+export default function Input({ status = 'text', isDark = false }: InputProps) {
+  const searchInputId = useId();
+  const textInputId = useId();
+  const textDisabledInputId = useId();
+
+  if (status === 'search') {
+    return (
+      <div className={getInputStatus(status, isDark)}>
+        <label htmlFor={searchInputId} className="sr-only" aria-describedby={searchInputId}>검색어</label>
+        <input type="text" id={searchInputId} className="body-sm" placeholder="검색어를 입력해 주세요." />
+      </div>
+    );
+  }
+
+  if (status === 'disabled') {
+    return (
+      <div className={getInputStatus(status, isDark)}>
+        <label htmlFor={textDisabledInputId} className="body-sm" aria-describedby={textDisabledInputId}>타이틀</label>
+        <input type="text" id={textDisabledInputId} className="body-sm" placeholder="내용을 입력해 주세요." disabled />
+        <span className="body-xs">내용을 입력해 주세요.</span>
+      </div>
+    );
   }
 
   return (
-    <>
-      {
-        status === 'search'
-          ?
-          <div className={getInputStatus()}>
-            <label htmlFor="search" className="sr-only">검색어</label>
-            <input type="text" id="search" className="body-sm" placeholder="검색어를 입력해 주세요." />
-          </div>
-          :
-          status === 'disabled'
-            ?
-            <div className={getInputStatus()}>
-              <label htmlFor="text-disabled" className="body-sm">타이틀</label>
-              <input type="text" id="text-disabled" className="body-sm" placeholder="내용을 입력해 주세요." disabled />
-              <span className="body-xs">내용을 입력해 주세요.</span>
-            </div>
-            :
-            <div className={getInputStatus()}>
-              <label htmlFor="text" className="body-sm">타이틀</label>
-              <input type="text" id="text" className="body-sm" placeholder="내용을 입력해 주세요." />
-              <span className="body-xs">내용을 입력해 주세요.</span>
-            </div>
-      }
-    </>
-  )
+    <div className={getInputStatus(status, isDark)}>
+      <label htmlFor={textInputId} className="body-sm" aria-describedby={textInputId}>타이틀</label>
+      <input type="text" id={textInputId} className="body-sm" placeholder="내용을 입력해 주세요." />
+      <span className="body-xs">내용을 입력해 주세요.</span>
+    </div>
+  );
 }
