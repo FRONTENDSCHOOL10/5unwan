64ea713eb906eca2ec02981003ec13167b5f1f9f
arayofseonshinse <arayofseonshine@naver.com>
2024-09-11 09:29:07 +0900

    Style: 회원 탈퇴 페이지 스타일링

--- src/routes/MyPage/DeleteAccount/deleteComplete.tsx
+++ src/routes/MyPage/DeleteAccount/deleteComplete.tsx
@@ -2,12 +2,15 @@ import { useNavigate } from "react-router-dom";
 import LargeButton from "@/components/Buttons/PrimaryButton/largeButton";
 import styles from "./deleteComplete.module.css";
 
-export default function DeleteComplete({ username }: { username: string }) {
-  const navigate = useNavigate();
+
+export default function DeleteComplete() {
+	const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <h1>{username}님의 회원탈퇴가 완료되었습니다.</h1>
+    <h1 className={styles.title}>
+	회원 탈퇴가 완료되었습니다.
+	</h1>
       <LargeButton onClick={() => navigate("/login")}>확인</LargeButton>
     </div>
   );
