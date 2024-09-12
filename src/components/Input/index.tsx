26973e31ec58969ba91c378c7731f62430fc01b2
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-12 10:27:29 +0900

    Refactor: input컴포넌트 리팩토링

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,16 @@
 import { useId } from 'react';
 import styles from './input.module.css';
+import SVGIcon from '../SVGicon';
 
 interface InputProps {
   status?: string,
-  isDark?: boolean
+  isDark?: boolean,
+  placeholder?: string,
+  title?: string,
+  titleHide?: boolean,
+  text?: string,
+  textHide?: boolean,
+  buttonName?: string,
 }
 
 function getInputStatus(status :string, isDark :boolean) {
@@ -14,14 +21,23 @@ function getInputStatus(status :string, isDark :boolean) {
       return `${styles["input-search"]} ${darkClass}`;
     case 'disabled':
       return `${styles["input-text"]} ${darkClass}`;
-    case 'text':
-      return `${styles["input-text"]} ${darkClass}`;
+    case 'button':
+      return `${styles["input-button"]} ${darkClass}`;
     default:
       return `${styles["input-text"]} ${darkClass}`;
   }
 }
 
-export default function Input({ status = 'text', isDark = false }: InputProps) {
+export default function Input({
+  status = "text",
+  title = "타이틀",
+  titleHide = false,
+  isDark = false,
+  placeholder = "내용을 입력해 주세요.",
+  text = "내용을 입력해 주세요.",
+  textHide = true,
+  buttonName = '버튼'
+}: InputProps) {
   const searchInputId = useId();
   const textInputId = useId();
   const textDisabledInputId = useId();
@@ -29,8 +45,11 @@ export default function Input({ status = 'text', isDark = false }: InputProps) {
   if (status === 'search') {
     return (
       <div className={getInputStatus(status, isDark)}>
-        <label htmlFor={searchInputId} className="sr-only">검색어</label>
-        <input type="text" id={searchInputId} className="body-sm" placeholder="검색어를 입력해 주세요." />
+        <label htmlFor={searchInputId} className="sr-only">{title}</label>
+        <input type="text" id={searchInputId} className="body-sm-medium" placeholder={placeholder} />
+        <button type="button" className={styles["search-icon"]}>
+          <SVGIcon iconId="iconSearch" width={14} height={14} />
+        </button>
       </div>
     );
   }
@@ -38,18 +57,31 @@ export default function Input({ status = 'text', isDark = false }: InputProps) {
   if (status === 'disabled') {
     return (
       <div className={getInputStatus(status, isDark)}>
-        <label htmlFor={textDisabledInputId} className="body-sm">타이틀</label>
-        <input type="text" id={textDisabledInputId} className="body-sm" placeholder="내용을 입력해 주세요." disabled />
-        <span className="body-xs">내용을 입력해 주세요.</span>
+        <label htmlFor={textDisabledInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
+        <input type="text" id={textDisabledInputId} className="body-sm-medium" placeholder={placeholder} disabled />
+        {textHide ? <span className="body-xs">{text}</span> : null}
+      </div>
+    );
+  }
+
+  if (status === 'button') {
+    return (
+      <div className={getInputStatus(status, isDark)}>
+        <label htmlFor={textInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
+        <form>
+          <input type="text" id={textInputId} className="body-sm-medium" placeholder={placeholder} />
+          <button type="button" className="body-sm-medium">{buttonName}</button>
+        </form>
+        {textHide ? <span className="body-xs">{text}</span> : null}
       </div>
     );
   }
 
   return (
     <div className={getInputStatus(status, isDark)}>
-      <label htmlFor={textInputId} className="body-sm">타이틀</label>
-      <input type="text" id={textInputId} className="body-sm" placeholder="내용을 입력해 주세요." />
-      <span className="body-xs">내용을 입력해 주세요.</span>
+      <label htmlFor={textInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
+      <input type="text" id={textInputId} className="body-sm-medium" placeholder={placeholder} />
+      {textHide ? <span className="body-xs">{text}</span> : null}
     </div>
   );
 }
