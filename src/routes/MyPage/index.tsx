01b0986a4fb5a53d887f49631b336bf3a257f77b
arayofseonshinse <arayofseonshine@naver.com>
2024-09-12 09:46:39 +0900

    Refactor: npm run build 오류 수정

--- src/routes/MyPage/index.tsx
+++ src/routes/MyPage/index.tsx
@@ -6,8 +6,6 @@ import { useState } from "react";
 import styles from "./index.module.css";
 import {
   PrimaryLargeButton,
-  PrimaryMediumButton,
-  PrimaryMiniButton,
 } from "@/components/Buttons/PrimaryButton/index";
 import { TertiaryMiniButton } from "@/components/Buttons/TertiaryButton/index";
 
