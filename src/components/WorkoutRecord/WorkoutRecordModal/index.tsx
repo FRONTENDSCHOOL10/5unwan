ef0d0c83a9096813b86f446a2377983fb9c9c91e
seulbinim <skfo0827@naver.com>
2024-09-17 16:14:45 +0900

    Feat: workout 기능구현 및 스타일링

---
+++ src/components/WorkoutRecord/WorkoutRecordModal/index.tsx
@@ -0,0 +1,42 @@
+// https://codesandbox.io/p/devbox/drawer-without-scale-forked-kxh9j5?file=%2Fapp%2Fmy-drawer.tsx%3A14%2C22
+
+import { Drawer } from "vaul";
+import { useState } from "react";
+import { WorkoutRecordForm } from "@/components/WorkoutRecord/WorkoutRecordForm";
+import styles from "@/components/WorkoutRecord/WorkoutRecordModal/workoutRecordModal.module.css";
+
+import SVGIcon from "@/components/SVGicon";
+import iconstyles from "@/components/SVGicon/styles.module.css";
+
+export function WorkoutRecordModal() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
+  return (
+    <Drawer.Root dismissible={false} open={modalOpen}>
+      <Drawer.Trigger asChild>
+        <button onClick={handleOpen} type="button" className={iconstyles["gnb-icon-edit"]}>
+          <SVGIcon iconId="iconSignatureSmall" width={20} height={20} color="var(--white)"/>
+        </button>
+      </Drawer.Trigger>
+      <Drawer.Portal>
+        <Drawer.Overlay className={styles.overlay} />
+        <Drawer.Content className={styles.outer}>
+          <div className={styles.inner}>
+            <span className={styles.handle}></span>
+              <Drawer.Title>
+                <p className="body-xl-bold">득근 기록하기</p>
+                <br />
+              </Drawer.Title>
+                <WorkoutRecordForm
+                  onSuccess={handleClose}
+                  onCancel={handleClose}
+                />
+          </div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  );
+}
+
