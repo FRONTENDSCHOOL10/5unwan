26fbb7e7c34364996c16a8bf617ea66734504083
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-19 21:09:21 +0900

    Refactor: Input컴포넌트 수정

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,129 +4,139 @@ import SVGIcon from '@/components/SVGicon';
 import {PrimaryMiniButton} from "@/components/Buttons/PrimaryButton/index";
 
 interface InputProps {
-  status?: string,
-  isDark?: boolean,
-  placeholder?: string,
-  title?: string,
-  titleHide?: boolean,
-  text?: string,
-  textHide?: boolean,
-  buttonName?: string,
-  value?: string,
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  type?: "text" | "search" | "email" | "password" | "checked" | "radio",
+  disabled?: boolean,
+  labelTitle?: string,
+  labelHide?: boolean,
+  placeholder?: string
+  errorText?: string,
+  errorTextHide?: boolean,
+  min?: number,
+  max?: number,
+  value?: string | undefined,
   onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void,
-  minLength?: number,
-  maxLength?: number,
-}
-
-function getInputStatus(status :string, isDark :boolean) {
-  const darkClass = isDark ? styles["is-dark"] : '';
-
-  switch (status) {
-    case 'search':
-      return `${styles["input-search"]} ${darkClass}`;
-    case 'disabled':
-      return `${styles["input-text"]} ${darkClass}`;
-    case 'button':
-      return `${styles["input-button"]} ${darkClass}`;
-    default:
-      return `${styles["input-text"]} ${darkClass}`;
-  }
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  isDark?: boolean,
+  button?: boolean,
+  buttonName?: string,
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void,
 }
 
 export default function Input({
-  status = "text",
-  title = "타이틀",
-  titleHide = false,
-  isDark = false,
+  type = "text",
+  disabled = false,
+  labelTitle = "타이틀",
+  labelHide = false,
   placeholder = "내용을 입력해 주세요.",
-  text = "내용을 입력해 주세요.",
-  textHide = false,
-  buttonName = '버튼',
-  value,
-  minLength,
-  maxLength,
-  onChange,
+  errorText = "내용을 입력해 주세요.",
+  errorTextHide = false,
+  min,
+  max,
   onSubmit,
+  value,
+  onChange,
+  isDark = false,
+  button = false,
+  buttonName = '버튼',
+  onKeyDown,
 }: InputProps) {
-  const searchInputId = useId();
-  const textInputId = useId();
-  const textDisabledInputId = useId();
+  const inputId = useId();
 
-  if (status === 'search') {
+  if (disabled) {
     return (
-      <div className={getInputStatus(status, isDark)}>
-        <form action="" onSubmit={onSubmit}>
-          <label htmlFor={searchInputId} className="sr-only">{title}</label>
-          <input
-            type="text"
-            id={searchInputId}
-            className="body-sm-medium"
-            placeholder={placeholder}
-            value={value}
-            minLength={minLength}
-            maxLength={maxLength}
-            onChange={onChange} />
-          <button type="submit" className={styles["search-icon"]}>
-            <SVGIcon iconId="iconSearch" width={14} height={14} />
-          </button>
+      <div className={`${styles["input"]} ${isDark ? styles["is-dark"] : ""}`}>
+        <form onSubmit={onSubmit}>
+          <label htmlFor={inputId} className={`body-sm-medium ${labelHide ? "sr-only" : ""}`}>{labelTitle}</label>
+          <div className={styles["input-wrapper"]}>
+            <input
+              type={type}
+              id={inputId}
+              className="body-sm-medium"
+              placeholder={placeholder}
+              minLength={min}
+              maxLength={max}
+              value={value}
+              onChange={onChange}
+              onKeyDown={onKeyDown}
+              disabled
+            />
+          </div>
+          <span className={errorTextHide ? "sr-only" : "body-xs"}>{errorText}</span>
         </form>
       </div>
     )
   }
 
-  if (status === 'disabled') {
+  if (button) {
     return (
-      <div className={getInputStatus(status, isDark)}>
+      <div className={`${styles["input"]} ${isDark ? styles["is-dark"] : ""}`}>
         <form onSubmit={onSubmit}>
-          <label htmlFor={textDisabledInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
-          <input
-            type="text"
-            id={searchInputId}
-            className="body-sm-medium"
-            placeholder={placeholder}
-            value={value}
-            minLength={minLength}
-            maxLength={maxLength}
-            onChange={onChange}
-            disabled
-          />
-          <span className={textHide ? "sr-only" : "body-xs"}>{text}</span>
-        </form>
-      </div>
-    );
-  }
-
-  if (status === 'button') {
-    return (
-      <div className={getInputStatus(status, isDark)}>
-        <form onSubmit={onSubmit}>
-          <label htmlFor={textInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
+          <label htmlFor={inputId} className={`body-sm-medium ${labelHide ? "sr-only" : ""}`}>{labelTitle}</label>
           <div className={styles["input-wrapper"]}>
             <input
-              type="text"
-              id={searchInputId}
+              type={type}
+              id={inputId}
               className="body-sm-medium"
               placeholder={placeholder}
+              minLength={min}
+              maxLength={max}
               value={value}
-              minLength={minLength}
-              maxLength={maxLength}
               onChange={onChange}
+              onKeyDown={onKeyDown}
             />
             <PrimaryMiniButton type="submit">{ buttonName }</PrimaryMiniButton>
           </div>
-          <span className={textHide ? "sr-only" : "body-xs"}>{text}</span>
+          <span className={errorTextHide ? "sr-only" : "body-xs"}>{errorText}</span>
         </form>
       </div>
-    );
+    )
+  }
+
+  if (type === "search") {
+    return (
+      <div className={`${styles["input"]} ${isDark ? styles["is-dark"] : ""}`}>
+        <form onSubmit={onSubmit}>
+          <label htmlFor={inputId} className={`body-sm-medium ${labelHide ? "sr-only" : ""}`}>{labelTitle}</label>
+          <div className={styles["input-wrapper"]}>
+            <button type="submit" className={styles["search-icon"]}>
+              <SVGIcon iconId="iconSearch" width={14} height={14} />
+            </button>
+            <input
+              type={type}
+              id={inputId}
+              className="body-sm-medium"
+              placeholder={placeholder}
+              minLength={min}
+              maxLength={max}
+              value={value}
+              onChange={onChange}
+              onKeyDown={onKeyDown}
+            />
+          </div>
+          <span className={errorTextHide ? "sr-only" : "body-xs"}>{errorText}</span>
+        </form>
+      </div>
+    )
   }
 
   return (
-    <div className={getInputStatus(status, isDark)}>
-      <label htmlFor={textInputId} className={titleHide ? "sr-only" : ''}>{title}</label>
+    <div className={`${styles["input"]} ${isDark ? styles["is-dark"] : ""}`}>
       <form onSubmit={onSubmit}>
-        <input type="text" id={textInputId} className="body-sm-medium" placeholder={placeholder} onChange={onChange} />
-        <span className={textHide ? "sr-only" : ''}>{text}</span>
+        <label htmlFor={inputId} className={labelHide ? "sr-only" : ""}>{labelTitle}</label>
+        <div className={styles["input-wrapper"]}>
+          <input
+            type={type}
+            id={inputId}
+            className="body-sm-medium"
+            placeholder={placeholder}
+            minLength={min}
+            maxLength={max}
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+          />
+        </div>
+        <span className={errorTextHide ? "sr-only" : "body-xs"}>{errorText}</span>
       </form>
     </div>
   );
