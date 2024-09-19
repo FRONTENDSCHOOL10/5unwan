080d6f45927dee47b46f1f3109ec5b953b31910f
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-19 13:34:25 +0900

    Feat: Home 컴포넌트 프로필 - 프로필 이미지 클릭 시 마이페이지, 로그아웃 메뉴 드랍다운

--- src/routes/Home/UserInfo/index.tsx
+++ src/routes/Home/UserInfo/index.tsx
@@ -1,15 +1,36 @@
-import { User, getPbImageUrl } from '@/api/pocketbase';
+import { useState } from 'react';
+import { User, getPbImageUrl, logout } from '@/api/pocketbase';
 import styles from './userInfo.module.css';
 import classNames from "classnames";
 import { useDarkMode } from "@/components/DarkModeContext/DarkModeContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 interface userProps {
   user: User;
+  show: boolean
 }
 
 export default function UserInfo({ user }: userProps) {
   const { isDark } = useDarkMode();
   const getUserAvatar = getPbImageUrl(user, user.avatar) ?? undefined;
+  const [show, setShow] = useState(false);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const logoutMutation = useMutation({
+    mutationFn: async () => {
+      logout();
+    },
+    onSuccess() {
+      queryClient.clear();
+      navigate("/login");
+    },
+  });
+  
+  function handleMenuClick() {
+    setShow(!show);
+  }
 
   return (
     <div className={classNames(styles.wrapper, { [styles["is-dark"]]: isDark })}>
@@ -17,8 +38,14 @@ export default function UserInfo({ user }: userProps) {
         <h1><strong className={styles["user-name"]}>{user.nickname}</strong>님 안녕하세요!</h1>
         <p>오늘도 <strong>득근득근!</strong></p>
       </div>
-      <div className={styles["user-profile"]}>
-        <img src={user.avatar ? getUserAvatar :'/avatar-placeholder.webp'} alt={`${user.nickname}님의 프로필`} />
+      <div className={styles["user-profile-wrapper"]}>
+        <button type="button" className={styles["user-profile"]} onClick={handleMenuClick}>
+          <img src={user.avatar ? getUserAvatar :'/avatar-placeholder.webp'} alt={`${user.nickname}님의 프로필`} />
+        </button>
+        <ul className={show ? styles["is-active"] : ''}>
+          <li className="body-sm-regular"onClick={() => navigate('/my-page')}>마이페이지</li>
+          <li className="body-sm-regular" onClick={async () => { await logoutMutation.mutateAsync() }}>로그아웃</li>
+        </ul>
       </div>
     </div>
   )
