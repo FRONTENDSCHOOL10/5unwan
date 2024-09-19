c4a6bf04b0528ea3fa702b16ab6f4efff0a5d377
seulbinim <skfo0827@naver.com>
2024-09-19 17:55:27 +0900

    Style: content-wrapper

--- src/routes/Onboarding/OnboardingBasic/index.tsx
+++ src/routes/Onboarding/OnboardingBasic/index.tsx
@@ -2,6 +2,7 @@ import { UserContext } from "@/routes/PrivateRoute";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { OnboardingBasicForm } from "@/routes/Onboarding/OnboardingBasic/OnboardingBasicForm";
 import { ONBOARDING_STEPS } from "@/utils/onboarding";
+import styles from "@/routes/Onboarding/OnboardingBasic/style.module.css";
 
 export function Component() {
   const { user } = useOutletContext<UserContext>();
@@ -9,7 +10,7 @@ export function Component() {
   const currentStep = ONBOARDING_STEPS.indexOf(currentRoute);
   const navigate = useNavigate();
   return (
-    <div>
+    <div className={styles["content-wrapper"]}>
       <OnboardingBasicForm
         onSuccess={() => {
           if (currentStep === ONBOARDING_STEPS.length - 1) {
