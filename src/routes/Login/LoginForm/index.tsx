d87372e04f57e94bbed82dc9d93696fcd4c0fabb
kimHyeonJu <kimhyesom1202@gmail.com>
2024-09-20 13:55:04 +0900

    Style: Start, Login 페이지, LoginForm컴포넌트 마크업 및 스타일링

--- src/routes/Login/LoginForm/index.tsx
+++ src/routes/Login/LoginForm/index.tsx
@@ -1,15 +1,16 @@
+import styles from "./loginForm.module.css";
+import { useState } from "react";
 import { loginWithPassword } from "@/api/pocketbase";
-import { useMutation } from "@tanstack/react-query";
 import { ClientResponseError } from "pocketbase";
-import React, { useId, useState } from "react";
-import { Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import Input from "@/components/Input";
+import { PrimaryLargeButton } from "@/components/Buttons/PrimaryButton/index";
 
 export type LoginFormProps = {
   onSuccess: () => void | Promise<void>;
 };
 
 export function LoginForm({ onSuccess }: LoginFormProps) {
-  const id = useId();
   const [formData, setFormData] = useState(() => {
     return {
       email: "",
@@ -45,53 +46,49 @@ export function LoginForm({ onSuccess }: LoginFormProps) {
     });
   };
 
+  function showErrorMessage() {
+    if (loginMutation.error instanceof ClientResponseError && loginMutation.error.response.code === 400) {
+      return <strong className={`body-xm-medium ${styles.errorText}`}>이메일 또는 비밀번호를 확인해주세요.</strong>
+    } else if (loginMutation.isError) {
+      return <strong className={`body-xm-medium ${styles.errorText}`}>알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요</strong>
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div role="group">
-        <label htmlFor={`${id}-email`}>
-          <h2 className="sr-only">이메일</h2>
-        </label>
-        <input
-          id={`${id}-email`}
-          name="email"
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <Input
           type="email"
-          placeholder="이메일을 입력해주세요."
+          name="email"
+          labelTitle="이메일"
+          placeholder="이메일을 입력해 주세요."
+          errorText="내용을 입력해 주세요."
           value={formData.email}
           onChange={handleUpdateFormData}
         />
-      </div>
-      <div role="group">
-        <label htmlFor={`${id}-password`}>
-          <h2 className="sr-only">비밀번호</h2>
-        </label>
-        <input
-          id={`${id}-password`}
-          name="password"
+        <Input
           type="password"
-          placeholder="비밀번호를 입력해주세요"
+          name="password"
+          labelTitle="비밀번호"
+          placeholder="비밀번호를 입력해 주세요."
+          errorText="내용을 입력해 주세요."
           value={formData.password}
           onChange={handleUpdateFormData}
         />
-      </div>
-      <div>
-        <Link to="#">비밀번호 찾기</Link>
-      </div>
-      <button
-        type="submit"
-        disabled={
-          !formData.email.trim() ||
-          !formData.password.trim() ||
-          loginMutation.isPending
+        <PrimaryLargeButton
+          type="submit"
+          disabled={
+            !formData.email.trim() ||
+            !formData.password.trim() ||
+            loginMutation.isPending
+          }
+        >로그인</PrimaryLargeButton>
+        {
+          showErrorMessage()
         }
-      >
-        로그인
-      </button>
-      {loginMutation.error instanceof ClientResponseError &&
-      loginMutation.error.response.code === 400
-        ? "이메일 또는 비밀번호를 확인해주세요."
-        : loginMutation.isError
-        ? "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요"
-        : null}
-    </form>
-  );
+      </form>
+    </div>
+  )
 }
